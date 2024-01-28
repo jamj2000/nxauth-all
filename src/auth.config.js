e8f972db4158922413448760ee4f7b46a81809f1
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2024-01-29 00:00:42 +0100

    add prisma to auth.config.js

--- src/auth.config.js
+++ src/auth.config.js
@@ -2,6 +2,7 @@ import Credentials from "@auth/core/providers/credentials"
 import Google from "@auth/core/providers/google"
 import GitHub from '@auth/core/providers/github'
 import bcrypt from 'bcryptjs'
+import { prisma } from "@/lib/prisma"
 
 export default {
     providers: [
